b371e3f3c5292524b55878a10140bc5794cf0169
pranav02082003 <luckypranav47@gmail.com>
2024-12-29 23:55:21 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+// import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+
+
+function App() {
+  
+  return (
+    <div>
+      <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
